04130dc1dae7964c8c9aecdd170169b44943c64c
kenenisa <kenenisaboru998@gmail.com>
2026-04-01 19:32:57 +0100

    Update database.php: added PDO connection, error handling, and security

--- config/database.php
+++ config/database.php
@@ -3,19 +3,17 @@
 $host = 'localhost';
 $db   = 'dfcms';
 $user = 'root';
-$pass = '12345678'; // Found in XAMPP config.inc.php
-$charset = 'utf8mb4';
+$pass = '12345'; 
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
 $options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_SILENT,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
 ];
 
 try {
-     $pdo = new PDO($dsn, $user, $pass, $options);
-} catch (\PDOException $e) {
-     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (PDOException $e) {
+    $pdo = null;
 }
 ?>
